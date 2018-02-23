0277811d8f5421eb26cb0b4147b156ce7407e70e
Jan-Philipp <janweber95@outlook.com>
2018-02-23 11:44:08 +0100

    Add defects for initial import script

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,4 +2,10 @@ INSERT INTO USER (ID, USERNAME, PASSWORD) VALUES (1, 'Harald', '123456');
 INSERT INTO USER (ID, USERNAME, PASSWORD) VALUES (2, 'Werner', 'fussball');
 INSERT INTO USER (ID, USERNAME, PASSWORD) VALUES (3, 'Friedrich', 'Holsten');
 
-INSERT INTO DEFECT (ID, TITLE, DESCRIPTION, CREATION_DATE, CREATOR_ID, STATUS) VALUES (4, 'Lorem Ipsum', 'Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und Konsonantien leben die Blindtexte. Abgeschieden wohnen sie in Buchstabhausen an der Küste des Semantik, eines großen Sprachozeans. Ein kleines Bächlein namens Duden fließt durch ihren Ort und versorgt sie mit den nötigen Regelialien. Es ist ein paradiesmatisches Land, in dem einem gebratene Satzteile in den Mund fliegen. Nicht einmal von der allmächtigen Interpunktion werden die Blindtexte beherrscht – ein geradezu unorthographisches Leben.', '2018-02-22 11:55:00', 1, 'CREATED');
+INSERT INTO DEFECT (ID, TITLE, DESCRIPTION, CREATION_DATE, CREATOR_ID, STATUS) VALUES (4, 'Kein Titel', 'Beim Anzeigen von geschlossenen Defects wird der Titel nicht mehr angezeigt', '2018-02-19 11:55:00', 1, 'CREATED');
+INSERT INTO DEFECT (ID, TITLE, DESCRIPTION, CREATION_DATE, CREATOR_ID, STATUS) VALUES (5, 'Loginprobleme', 'User 3 und 4 funktionieren nicht', '2018-02-20 16:45:00', 2, 'REJECTED');
+INSERT INTO DEFECT (ID, TITLE, DESCRIPTION, CREATION_DATE, CREATOR_ID, STATUS) VALUES (7, 'Datenanzeige', 'Es wird nur ein Teil der darzustellenden Daten übermittelt', '2018-02-20 14:29:00', 2, 'REOPENED');
+INSERT INTO DEFECT (ID, TITLE, DESCRIPTION, CREATION_DATE, CREATOR_ID, STATUS) VALUES (8, 'Footer fehlt', 'Fußleiste mit Impressum wird nicht angezeigt', '2018-02-22 09:30:00', 3, 'RESOLVED');
+INSERT INTO DEFECT (ID, TITLE, DESCRIPTION, CREATION_DATE, CREATOR_ID, STATUS) VALUES (8, 'Geomapping fehlerhaft', 'Europakarte wird zur Hälfte abgeschnitten', '2018-02-22 09:30:00', 3, 'CLOSED');
+INSERT INTO DEFECT (ID, TITLE, DESCRIPTION, CREATION_DATE, CREATOR_ID, STATUS) VALUES (11, 'UI Probleme', 'Benutzeroberfläche flackert unregelmäßig beim scrollen', '2018-02-23 13:00:00', 1, 'IN_PROCESS');
+
