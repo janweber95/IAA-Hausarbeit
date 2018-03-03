cda5fec64752fd95f428bc2dc68b051b45c86794
Jan-Philipp <janweber95@outlook.com>
2018-03-03 03:33:10 +0100

    Add post request for defect change

--- src/main/resources/static/controller/DefectChangeController.js
+++ src/main/resources/static/controller/DefectChangeController.js
@@ -5,15 +5,28 @@
  * @author Jan-Philipp Weber
  */
 app.controller('DefectChangeController', function($scope, $http, $location, defectService,userService, defectStatusService) {
-
-    $scope.defectChanges = defectService.getDefect();
+    var defectChangeUrl = '/defects/';
+    $scope.defect = defectService.getDefect();
+    $scope.defectChange = {};
     $scope.changeStatus = defectStatusService.getDefectStatus();
+    $scope.defectChange.user = userService.getuser();
+    $scope.defectChange.previousStatus = $scope.defect.status;
+    +new Date;
 
     $scope.switchToDetail = function () {
         $location.path("/defectdetail");
     };
     
-    $scope.changeDefect = function () {
-
-    }
+    $scope.changeDefect = function (status) {
+        $scope.defectChange.newStatus = status;
+        $scope.defectChange.changeTime = Date.now();
+        $http.post(defectChangeUrl+$scope.defect.id, angular.toJson($scope.defectChange))
+            .then(function successCallback(response) {
+                alert("Die Änderung wurde erfolgreich durchgeführt")
+                switchToDetail();
+            }, function errorCallback(data, status , header) {
+                console.error(data, status, header);
+                alert("Hoppla, bitte kontrollieren Sie Ihre Eingaben und versuchen Sie es erneut!")
+            })
+    };
 });
